fe594b1aa8277c40b988d34e73ca81d771483970
charLin <118043579+Code-Min159@users.noreply.github.com>
2024-12-25 09:04:20 +0800

    Update subFactory.java

--- subFactory.java
+++ subFactory.java
@@ -2,6 +2,8 @@ public class subFactory extends factory{
     private String nickName;
     public float weight;
     //call father's name
+
+    //要修改的值可以在建構子這邊修改嗎?函示要到下面實作的地方才能修改?
     public subFactory(){
         this.nickName = "little momo" + super.factoryName;
     }
@@ -9,9 +11,14 @@ public class subFactory extends factory{
 }
 
 //修改定重
-public int deliveryWeight = (super.deliveryWeight+5); 
+public int deliverWeight = (super.deliverWeight +5); 
 
 //get father's method and chang the value
-public void setWeight{
-    weight = 2*super.getWeight;
+public String titleName;
+
+public void setName{
+    titleName = super.factoryName + "(" + nickName + ")";
+}
+public getName(){
+    return this.titleName;
 }
