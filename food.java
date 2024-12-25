b2d3324b766c3ac9963b2511a7c7c3a0a5abf0cc
charLin <118043579+Code-Min159@users.noreply.github.com>
2024-12-25 11:22:26 +0800

    Update food.java

--- food.java
+++ food.java
@@ -26,18 +26,19 @@ public class food{
             System.out.println("foodArea =" + addtiviteArea);
         }
     }
+    // when run the code, init this and put into memory without use new()..... 
+    static void sayHello{
+        System.out.println("hello, this is food.")
+    }
+    
+    // call funtion()-innerCheck
+    public void callInnerCheck(){
+        innerCheck check = new innerCheck();
+        check.foodArea();
+    }
+
 }
 
-// when run the code, init this and put into memory without use new()..... 
-static void sayHello{
-    System.out.println("hello, this is food.")
-}
-
-// call funtion()-innerCheck
-public void callInnerCheck(){
-    innerCheck check = new innerCheck();
-    check.foodArea();
-}
 
 
 
